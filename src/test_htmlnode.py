4a7553a636326002027a9890d94d7a28d9049f11
graham-br <grahamroberts31@gmail.com>
2024-03-19 09:18:39 -0600

    Adds LeafNode child class to HTMLNode and unit tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 class TestTextNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -11,6 +12,23 @@ class TestTextNode(unittest.TestCase):
         child_node = HTMLNode("a", "link for more info", None, {"href": "https://www.boot.dev", "target": "_blank"})
         node = HTMLNode("p", "text of the site here", child_node, {"class": "bold important-text color2"})
         self.assertEqual("HTMLNode(p, text of the site here, HTMLNode(a, link for more info, None, {'href': 'https://www.boot.dev', 'target': '_blank'}), {'class': 'bold important-text color2'})", repr(node))
+
+    def test_no_tag_to_html(self):
+        leaf_node = LeafNode(None, "Hello")
+        self.assertEqual(leaf_node.to_html(), "Hello")
+
+    def test_leaf_node_to_html(self):
+        leaf_node = LeafNode("a", "link to somewhere", {"href": "https://www.boot.dev"})
+        self.assertEqual(leaf_node.to_html(), '<a href="https://www.boot.dev">link to somewhere</a>')
+
+    def test_leaf_no_value(self):
+        leaf_node = LeafNode("p", None)
+        with self.assertRaises(ValueError) as cm:
+            leaf_node.to_html()
+        self.assertEqual(
+            "value required for LeafNode",
+            str(cm.exception)
+        )
 
 if __name__ == "__main__":
     unittest.main()
